23d9e7de14b9f0f1580cc3e619394ec2cdbecb49
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-10 01:38:05 +0300

    commit tree 275172eda4f82b8bb1f8c2a24f17923a6aa44d3c

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2, drivers.length);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (fare) => () => fare * fare;
+
+const fareDoubler = (fare) => 2 * fare;
+
+const fareTripler = (fare) => 3 * fare;
+
+const selectDifferentDrivers = (drivers, callback) => {
+  return callback === selectingDrivers[0]
+    ? selectingDrivers[0](drivers) //  ? callback(drivers)
+    : selectingDrivers[1](drivers); // : callback(drivers);
+};
